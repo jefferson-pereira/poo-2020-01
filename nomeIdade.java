8971f82f8ca17e468aa5a7c3a5d922517ad94284
Jefferson Pereira da Silva <jefferson_pereira@discente.ufg.br>
2020-10-13 22:47:11 -0300

    O que vale na vida não é o ponto de partida e sim a caminhada. Caminhando e semeando, no fim terás o que colher

---
+++ nomeIdade.java
@@ -0,0 +1,7 @@
+public class nomeIdade {
+    public static void main(String[] args) {
+        String nome = "Jefferson Pereira da Silva";
+        int idade = 27;
+        System.out.println(nome +"\n"+ idade + " anos");
+    }
+}
