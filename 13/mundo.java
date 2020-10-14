a7f70190904024d7c410a013be5a145eddafc7f2
Jefferson Pereira da Silva <jefferson_pereira@discente.ufg.br>
2020-10-13 22:58:21 -0300

    exercicio 13 POO corrigido

--- 13/mundo.java
+++ 13/mundo.java
@@ -1,4 +1,4 @@
-public class teste{
+public class mundo{
     public static void main(String[] args) {
         System.out.println("eae mundo");
     }
---
+++ 13/nome.java
@@ -0,0 +1,6 @@
+public class nome {
+    public static void main(String[] args) {
+        String Nome = "Jefferson Pereira da Silva";
+        System.out.println("Meu nome é: "+Nome);
+    }
+}
